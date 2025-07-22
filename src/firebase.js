7b2bb8188ce8d0dbbbb09f90bb97230b125ede49
Manas Kumar Arjya <132188377+ManasArjya@users.noreply.github.com>
2025-07-23 00:36:48 +0530

    Update firebase.js
    removed my own api id

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCw5lARRmD-H84Ye7WiW0YhPt3rrbAkGfs",
-  authDomain: "blood-donation-tracker-6e354.firebaseapp.com",
-  projectId: "blood-donation-tracker-6e354",
-  storageBucket: "blood-donation-tracker-6e354.firebasestorage.app",
-  messagingSenderId: "632921416734",
-  appId: "1:632921416734:web:07daa118c8c56dfb3f7a0f"
+  apiKey: "Your Key",
+  authDomain: "Your firebase domain",
+  projectId: "your project id",
+  storageBucket: "your storage bucket",
+  messagingSenderId: "your sender id",
+  appId: "1your app id"
 };
 
 const app = initializeApp(firebaseConfig);
